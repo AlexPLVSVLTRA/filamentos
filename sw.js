c44bfeeac96557baa2114733025efe98d43c815f
AlexPLVSVLTRA <53815073+AlexPLVSVLTRA@users.noreply.github.com>
2026-07-06 21:43:36 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = '3dream-v1';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.4/chart.umd.min.js'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (e.request.method === 'GET' && res.ok) {
+        const clon = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clon));
+      }
+      return res;
+    }))
+  );
+});
