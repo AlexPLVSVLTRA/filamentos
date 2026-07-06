4f1761ae59e71dbe933ab9851647caa98a85dc9e
AlexPLVSVLTRA <53815073+AlexPLVSVLTRA@users.noreply.github.com>
2026-07-06 22:25:10 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,24 @@
-const CACHE = '3dream-v2';
+const CACHE = '3dream-v3';
 const FILES = [
   './',
   './index.html',
   './manifest.json',
   './icon-192.png',
-  './icon-512.png',
+  './icon-512.png'
+];
+const CDN = [
+  'https://cdn.jsdelivr.net/npm/chart.js@4.4.4',
   'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.4/chart.umd.min.js'
 ];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+  e.waitUntil(
+    caches.open(CACHE).then(c =>
+      c.addAll(FILES)
+        // los CDN se cachean si se puede, pero un fallo no bloquea la instalación
+        .then(() => Promise.allSettled(CDN.map(u => c.add(u))))
+    ).then(() => self.skipWaiting())
+  );
 });
 
 self.addEventListener('activate', e => {
